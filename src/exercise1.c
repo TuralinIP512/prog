2a494c52b6590c39147d444694e0c0ea2c6d209d
Туралин А.С. ИП-512 <ttt8641f@yandex.ru>
2025-10-26 22:21:46 +0700

    2

--- src/exercise1.c
+++ src/exercise1.c
@@ -4,8 +4,6 @@ int main(int argc, char** argv) {
     
     float array[10];
 
-    printf("Введите десять вещественных чисел:\n");
-
     for(int i = 0; i < 10; i++){
         scanf("%f", &array[i]);
     }
