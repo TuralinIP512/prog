1b5e27fb7020c50ef9d80c45c043c935a0a58148
Туралин А.С. ИП-512 <ttt8641f@yandex.ru>
2025-10-26 22:48:45 +0700

    5

--- src/exercise1.c
+++ src/exercise1.c
@@ -9,13 +9,20 @@ int main() {
 
     long long sum_cents = 0;
     for(int i = 0; i < 10; i++){
-        
-        sum_cents += (long long)(array[i] * 100 + 0.5);
+        double value = array[i] * 100;
+        if (value >= 0) {
+            sum_cents += (long long)(value + 0.5);
+        } else {
+            sum_cents += (long long)(value - 0.5);
+        }
     }
     
-  
-    long long aver_cents = (sum_cents + 5) / 10; 
-    
+    long long aver_cents;
+    if (sum_cents >= 0) {
+        aver_cents = (sum_cents + 5) / 10;
+    } else {
+        aver_cents = (sum_cents - 5) / 10;
+    }
     
     double result = aver_cents / 100.0;
     
